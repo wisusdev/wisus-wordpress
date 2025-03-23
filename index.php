8fc7db9a99e2f41795652b572662197f0caea7f2
Jesús Avelar <wisusdev@gmail.com>
2025-03-23 10:33:18 -0600

    Refactor layout structure and add sidebar support

--- index.php
+++ index.php
@@ -23,31 +23,45 @@ get_header();
 		<header>
 			<h1 class="page-title mb-4"><?php single_post_title(); ?></h1>
 		</header>
-
-		<div class="row row-cols-1 row-cols-md-<?php echo esc_attr( $columns ); ?> g-4">
-			<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) : ?>
-				<div class="col">
+		<div class="row">
+			<div class="col">
+				<div class="row row-cols-1 row-cols-md-<?php echo esc_attr( $columns ); ?> g-4">
 					<?php
-					the_post();
-					get_template_part( 'template-parts/content', get_post_type() );
+					endif;
+
+					/* Start the Loop */
+					while ( have_posts() ) : ?>
+						<div class="col">
+							<?php
+							the_post();
+							get_template_part( 'template-parts/content', get_post_type() );
+							?>
+						</div>
+					<?php
+					endwhile;
+					the_posts_navigation();
+					else :
+						get_template_part( 'template-parts/content', 'none' );
+					endif;
 					?>
 				</div>
-			<?php
-			endwhile;
-			the_posts_navigation();
-			else :
-				get_template_part( 'template-parts/content', 'none' );
-			endif;
-			?>
+			</div>
+			<?php if( $sidebarEnabled === 'yes' ) { ?>
+			<div class="col-4">
+				<div class="card">
+					<div class="card-body">
+						<?php
+							get_sidebar();
+						?>
+					</div>
+				</div>
+			</div>
+			<?php } ?>
 		</div>
+
+
+
 	</main><!-- #main -->
 
 <?php
-if( $sidebarEnabled === 'yes' ) {
-	get_sidebar();
-}
 get_footer();
