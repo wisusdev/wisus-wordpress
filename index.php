c674895c2b5973409735b27506fd4a3382a59352
Jesús Avelar <wisusdev@gmail.com>
2025-03-25 16:40:06 -0600

    Modificar la lógica de la clase principal para habilitar el contenedor fluido y ajustar la columna del sidebar según la configuración del tema

--- index.php
+++ index.php
@@ -13,9 +13,10 @@ get_header();
 
 <?php
 	$columns = get_theme_mod( 'wisus_columns', 3 );
-	$sidebarEnabled = get_theme_mod( 'wisus_enable_sidebar', 'yes' );
+    $sidebarEnabled = get_theme_mod( 'wisus_enable_sidebar');
+    $containerFluidEnabled = get_theme_mod( 'wisus_container_fluid');
 ?>
-	<main id="primary" class="site-main container pb-4">
+	<main id="primary" class="site-main pb-4 container<?php echo $containerFluidEnabled ? '-fluid' : ''; ?>">
 		<?php
 			if ( have_posts() ) :
 			if ( is_home()  ) :
@@ -24,7 +25,7 @@ get_header();
 			<h1 class="page-title mb-4"><?php single_post_title(); ?></h1>
 		</header>
 		<div class="row">
-			<div class="col">
+			<div class="mb-3 <?php echo $sidebarEnabled ? 'col-md-8' : 'col-md-12'; ?>">
 				<div class="row row-cols-1 row-cols-md-<?php echo esc_attr( $columns ); ?> g-4">
 					<?php
 					endif;
@@ -46,8 +47,8 @@ get_header();
 					?>
 				</div>
 			</div>
-			<?php if( $sidebarEnabled === 'yes' ) { ?>
-			<div class="col-4">
+			<?php if( $sidebarEnabled) { ?>
+			<div class="mb-3 col-md-4">
 				<div class="card">
 					<div class="card-body">
 						<?php
@@ -58,10 +59,6 @@ get_header();
 			</div>
 			<?php } ?>
 		</div>
-
-
-
 	</main><!-- #main -->
-
 <?php
 get_footer();
