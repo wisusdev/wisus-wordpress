27d2383f2420e0642f397d6333620fbcf05f06b9
Jesús Avelar <wisusdev@gmail.com>
2025-03-25 16:40:44 -0600

    Refactor single.php to improve sidebar handling and container fluidity

--- single.php
+++ single.php
@@ -11,46 +11,44 @@ get_header();
 ?>
 
 <?php
-	$sidebarEnabled = get_theme_mod( 'wisus_enable_sidebar', 'yes' );
+	$sidebarEnabled = get_theme_mod( 'wisus_enable_sidebar');
+    $containerFluidEnabled = get_theme_mod( 'wisus_container_fluid');
 ?>
-    <main id="primary" class="site-main single-container container py-4">
+    <main id="primary" class="site-main single-container py-4 container<?php echo $containerFluidEnabled ? '-fluid' : ''; ?>">
         <div class="row">
-            <div class="col">
+            <div class="mb-3 <?php echo $sidebarEnabled ? 'col-md-8' : 'col-md-12'; ?>">
                 <?php
-                while ( have_posts() ) :
-                    the_post();
+                    while ( have_posts() ) :
+                        the_post();
 
-                    get_template_part( 'template-parts/content' );
+                        get_template_part( 'template-parts/content' );
 
-                    ?>
-                        <div class="d-grid gap-2 d-flex justify-content-between p-3 bg-body-secondary rounded mb-3">
-                            <div><?php echo get_next_post_link('%link') ?></div>
-                            <div><?php echo get_previous_post_link('%link') ?></div>
-                        </div>
-                    <?php
+                        ?>
+                            <div class="d-grid gap-2 d-flex justify-content-between p-3 bg-body-secondary rounded mb-3">
+                                <div><?php echo get_next_post_link('%link') ?></div>
+                                <div><?php echo get_previous_post_link('%link') ?></div>
+                            </div>
+                        <?php
 
-                    // If comments are open or we have at least one comment, load up the comment template.
-                    if ( comments_open() || get_comments_number() ) :
-                        comments_template();
-                    endif;
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif;
 
-                endwhile; // End of the loop.
+                    endwhile; // End of the loop.
                 ?>
-            </div>
-            <?php if( $sidebarEnabled === 'yes' ) { ?>
-                <div class="col-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <?php
-                                get_sidebar();
-                            ?>
+
+				<?php if( $sidebarEnabled) { ?>
+                    <div class="mb-3 col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+								<?php get_sidebar(); ?>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php } ?>
+				<?php } ?>
+            </div>
         </div>
-
     </main><!-- #main -->
-
 <?php
 get_footer();
