11231918916f11dc3c2636af4452a478f2830a55
Jesús Avelar <wisusdev@gmail.com>
2025-03-25 17:54:29 -0600

    Refactor la estructura de page.php para mejorar la organización del código y la flexibilidad del diseño

--- page.php
+++ page.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package wisus
- */
-
 get_header();
 ?>
 
@@ -19,33 +6,34 @@ get_header();
     $sidebarEnabled = get_theme_mod( 'wisus_enable_sidebar');
     $containerFluidEnabled = get_theme_mod( 'wisus_container_fluid');
 ?>
-	<main id="primary" class="page-main py-4 container<?php echo $containerFluidEnabled ? '-fluid' : ''; ?>">
+<div class="d-flex flex-column">
+    <main id="primary" class="page-main flex-grow-1 py-4 container<?php echo $containerFluidEnabled ? '-fluid' : ''; ?>">
         <div class="row">
             <div class="mb-3 <?php echo $sidebarEnabled ? 'col-md-8' : 'col-md-12'; ?>">
-                <?php
-                while ( have_posts() ) :
-                    the_post();
+				<?php
+				while ( have_posts() ) :
+					the_post();
 
-                    get_template_part( 'template-parts/content', 'page' );
+					get_template_part( 'template-parts/content', 'page' );
 
-                    // If comments are open or we have at least one comment, load up the comment template.
-                    if ( comments_open() || get_comments_number() ) :
-                        comments_template();
-                    endif;
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
-                endwhile; // End of the loop.
-                ?>
+				endwhile;
+				?>
             </div>
-            <?php if( $sidebarEnabled) { ?>
-            <div class="mb-3 col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <?php get_sidebar(); ?>
+			<?php if( $sidebarEnabled) { ?>
+                <div class="mb-3 col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+							<?php get_sidebar(); ?>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <?php } ?>
+			<?php } ?>
         </div>
-	</main><!-- #main -->
-<?php
-get_footer();
+    </main>
+</div>
+
+<?php get_footer(); ?>
