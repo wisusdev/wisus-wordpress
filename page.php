307ce229fc5ebac3ba731a74ad8e33525a174eed
Jesús Avelar <wisusdev@gmail.com>
2025-03-25 16:40:20 -0600

    Agregar soporte para sidebar y contenedor fluido en la plantilla de página

--- page.php
+++ page.php
@@ -15,23 +15,37 @@
 get_header();
 ?>
 
-	<main id="primary" class="page-main py-4">
-		<?php
-		while ( have_posts() ) : 
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
 <?php
-get_sidebar();
+    $sidebarEnabled = get_theme_mod( 'wisus_enable_sidebar');
+    $containerFluidEnabled = get_theme_mod( 'wisus_container_fluid');
+?>
+	<main id="primary" class="page-main py-4 container<?php echo $containerFluidEnabled ? '-fluid' : ''; ?>">
+        <div class="row">
+            <div class="mb-3 <?php echo $sidebarEnabled ? 'col-md-8' : 'col-md-12'; ?>">
+                <?php
+                while ( have_posts() ) :
+                    the_post();
+
+                    get_template_part( 'template-parts/content', 'page' );
+
+                    // If comments are open or we have at least one comment, load up the comment template.
+                    if ( comments_open() || get_comments_number() ) :
+                        comments_template();
+                    endif;
+
+                endwhile; // End of the loop.
+                ?>
+            </div>
+            <?php if( $sidebarEnabled) { ?>
+            <div class="mb-3 col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <?php get_sidebar(); ?>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+	</main><!-- #main -->
+<?php
 get_footer();
