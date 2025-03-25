9ac2cd6ba72c0eb26c594f27ff87027c1e5a0195
Jesús Avelar <wisusdev@gmail.com>
2025-03-25 17:52:37 -0600

    Agregar script personalizado para mejorar la funcionalidad del tema

--- functions.php
+++ functions.php
@@ -138,6 +138,7 @@ function wisus_scripts() {
 	wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), null, true );
 
 	wp_enqueue_script( 'wisus-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'wisus-custom-script', get_template_directory_uri() . '/assets/js/script.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
