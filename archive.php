23cae05b098729aa4c3a4f54e377daa85ead907a
Jesús Avelar <wisusdev@gmail.com>
2025-03-25 16:39:38 -0600

    Agregar soporte para columnas personalizables y contenedor fluido en la plantilla de archivo

--- archive.php
+++ archive.php
@@ -11,23 +11,28 @@ get_header();
 ?>
 
 <?php
-	$sidebarEnabled = get_theme_mod( 'wisus_enable_sidebar', 'yes' );
+    $columns = get_theme_mod( 'wisus_columns', 3 );
+	$sidebarEnabled = get_theme_mod( 'wisus_enable_sidebar' );
+    $containerFluidEnabled = get_theme_mod( 'wisus_container_fluid');
 ?>
-	<main id="primary" class="site-main archive-container container py-4">
+	<main id="primary" class="site-main archive-container py-4 container<?php echo $containerFluidEnabled ? '-fluid' : ''; ?>">
 		<div class="row">
-			<div class="col">
+            <div class="mb-3 <?php echo $sidebarEnabled ? 'col-md-8' : 'col-md-12'; ?>">
 				<?php if ( have_posts() ) : ?>
 
-					<header class="page-header">
+					<header class="page-header mb-4">
 						<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="archive-description">', '</div>' );
+                            the_archive_title( '<h1 class="page-title">', '</h1>' );
+                            the_archive_description( '<div class="archive-description">', '</div>' );
 						?>
-					</header><!-- .page-header -->
-
-					<?php
+					</header>
+                    <!-- .page-header -->
+                <div class="row row-cols-1 row-cols-md-<?php echo esc_attr( $columns ); ?> g-4">
+                <?php
 					/* Start the Loop */
-					while ( have_posts() ) :
+					while ( have_posts() ) : ?>
+                        <div class="col">
+                        <?php
 						the_post();
 
 						/*
@@ -36,20 +41,19 @@ get_header();
 						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
 						*/
 						get_template_part( 'template-parts/content', get_post_type() );
-
+                        ?>
+                        </div>
+                    <?php
 					endwhile;
-
 					the_posts_navigation();
-
 				else :
-
 					get_template_part( 'template-parts/content', 'none' );
-
 				endif;
 				?>
+                </div>
 			</div>
-			<?php if( $sidebarEnabled === 'yes' ) { ?>
-                <div class="col-4">
+			<?php if( $sidebarEnabled) { ?>
+                <div class="mb-3 col-md-4">
                     <div class="card">
                         <div class="card-body">
                             <?php
