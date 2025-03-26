c014cccf132296c3437d94cab012a328ce531d36
Jesús Avelar <wisusdev@gmail.com>
2025-03-26 11:54:47 -0600

    Refactor la función 'get_last_post' para utilizar WP_Query y mejorar la estructura del HTML de salida

--- inc/helper.php
+++ inc/helper.php
@@ -167,131 +167,10 @@ function get_popular_post(){
 	wp_reset_query();
 }
 
-function better_comments( $comment, $args, $depth ) {
-
-	// Get correct tag used for the comments
-	if ( 'div' === $args['style'] ) {
-		$tag       = 'div';
-		$add_below = 'comment';
-	} else {
-		$tag       = 'li';
-		$add_below = 'div-comment';
-	} 
-	
-	?>
-	
-	<<?php echo $tag; ?> <?php comment_class( empty( $args['has_children'] ) ? 'card mb-3 shadow' : 'parent card' ); ?> id="comment-<?php comment_ID() ?>">
-
-	<?php
-		// Switch between different comment types
-		switch ( $comment->comment_type ) :
-			case 'pingback' :
-			case 'trackback' : 
-	?>
-		<div class="pingback-entry"><span class="pingback-heading"><?php esc_html_e( 'Pingback:', 'textdomain' ); ?></span> <?php comment_author_link(); ?></div>
-	<?php
-		break;
-		default :
-
-		if ( 'div' != $args['style'] ) { ?>
-			<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
-		<?php } ?>
-			<div class="comment-author vcard card-header d-flex justify-content-between">
-				<div class="username-avatar">
-					<?php
-						// Display avatar unless size is set to 0
-						if ( $args['avatar_size'] != 0 ) {
-							$avatar_size = ! empty( $args['avatar_size'] ) ? $args['avatar_size'] : 70; // set default avatar size
-							
-							echo get_avatar( $comment, $avatar_size, '', '', array('class' => 'rounded me-3') );
-						}
-						// Display author name
-						printf( __( '<cite class="author-link">%s</cite>', 'textdomain' ), get_comment_author_link() ); 
-					?>
-				</div>
-				
-				<div class="comment-meta commentmetadata">
-					<a class="text-decoration-none" href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>">
-						<?php
-							/* translators: 1: date, 2: time */
-							printf(
-								__( '%1$s', 'textdomain' ),
-								get_comment_date()
-							); 
-						?>
-					</a>
-				</div>
-
-				<div class="btn-edit">
-					<?php edit_comment_link( __( 'Editar', 'textdomain' ), '  ', '' ); ?>
-				</div>
-			</div><!-- .comment-author -->
-
-			<div class="comment-details card-body">
-				<div class="comment-text"><?php comment_text(); ?></div><!-- .comment-text -->
-				<?php
-					// Display comment moderation text
-					if ( $comment->comment_approved == '0' ) { ?>
-						<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'textdomain' ); ?></em><br/><?php
-					} 
-				?>
-			</div>
-
-			<div class="card-footer bg-white border-0">
-				<div class="reply float-end">
-					<?php
-						// Display comment reply link
-						comment_reply_link( array_merge( $args, array(
-							'add_below' => $add_below,
-							'depth'     => $depth,
-							'max_depth' => $args['max_depth'],
-							'class' 	=> 'btn btn-primary'
-						))); 
-					?>
-				</div>
-			</div>
-	<?php
-		if ( 'div' != $args['style'] ) { ?>
-			</div>
-		<?php }
-	// IMPORTANT: Note that we do NOT close the opening tag, WordPress does this for us
-		break;
-	endswitch; // End comment_type check.
-}
-
 /**
  * Hooks
  *----------------------------------------------------------------------------------------------------
  */
-
-// Se habilita el editor clasico de wordpress //
-//add_filter('use_block_editor_for_post', '__return_false');
-
-// Se añade las clase para los botones de previo y siguiente en la paginacion
-add_filter('next_posts_link_attributes', 'posts_link_attributes');
-add_filter('previous_posts_link_attributes', 'posts_link_attributes');
-function posts_link_attributes(): string {
-	return 'class="btn btn-secondary"';
-}
-
-// Se añaden las clases para los botones que aparecen en cada post entes del formulario de comentario
-function filter_single_post_pagination($output, $format, $link, $post){
-	if ($post != ''){
-		$title = get_the_title($post);
-		$url   = get_permalink($post->ID);
-		$class = 'btn btn-outline-secondary my-2 text-limit btn-block';
-		$rel   = 'prev';
-
-		if('next_post_link' === current_filter()){
-			$rel   = 'next';
-		}
-		return "<a href='$url' rel='$rel' class='$class'>$title</a>";
-	}
-
-	return null;
-}
-add_filter( 'next_post_link', 'filter_single_post_pagination', 10, 4);
-add_filter( 'previous_post_link', 'filter_single_post_pagination', 10, 4);
 
 
 // Se eliminan los p (parrafos) vacios
@@ -336,45 +215,37 @@ function get_last_post($atts, $content = null) {
     $output = '';
 
     // Obtener los posts
-    $posts = get_posts($args);
+    $query = new WP_Query($args);
 
-    foreach ($posts as $post) {
-        setup_postdata($post);
+    ob_start();
+	?>
+	<div class="mb-3 col-md-12">
+		<div class="row row-cols-1 row-cols-md-<?php echo esc_attr(get_theme_mod( 'wisus_columns', 3 )); ?> g-4">
+			<?php
+			if ($query->have_posts()):
+				while ($query->have_posts()):
+					$query->the_post();
+					?>
+					<div class="col">
+						<?php get_template_part('template-parts/content', get_post_type()); ?>
+					</div>
+					<?php
+				endwhile;
+			else:
+				get_template_part('template-parts/content', 'none');
+			endif;
+			wp_reset_postdata();
+			?>
+		</div>
+		<?php if ($query->found_posts > $atts['num']) { ?>
+			<div class="col-12 d-flex justify-content-center my-3">
+				<a href="<?php echo esc_url(site_url('/blog')); ?>" class="btn btn-primary">
+					Ver más entradas <i class="fas fa-arrow-circle-right ms-2"></i>
+				</a>
+			</div>
+		<?php } ?>
+	</div>
+	<?php
 
-        // Construir el HTML de salida
-        $output .= '
-            <div class="col-md-4 mb-3">
-                <div class="card h-100 shadow">
-                    <div class="card-header">
-                        <p class="text-capitalize m-1 fw-bold">' . get_the_date(get_option('date_format')) . '</p>
-                    </div>
-                    <div class="overflow-hidden p-0 image-post">
-                        <div class="post-thumbnail">
-                            <a href="' . get_the_permalink() . '">
-                                ' . get_the_post_thumbnail(null, '', array('class' => 'img-fluid')) . '
-                            </a>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <p class="entry-title card-title fw-bold">
-                            <a href="' . get_the_permalink() . '" rel="bookmark" class="text-decoration-none text-black">' . get_the_title() . '</a>
-                        </p>
-                        <p class="my-3 fw-normal">' . wp_trim_words(get_the_content(), 20) . '</p>
-                    </div>
-                </div>
-            </div>
-        ';
-		
-		if ($posts > $atts['num']){
-			$output .= '
-				<div class="col-12 d-flex justify-content-center my-3">
-						<a href="' .esc_url(site_url('/blog')) .'" class="btn btn-primary">Ver mas entradas <i class="fas fa-arrow-circle-right ms-2"></i></a>
-				</div>';
-		}
-    }
-
-    wp_reset_postdata();
-
-    // Devolver el HTML generado
-    return '<div class="row">' . $output . '</div>';
+	return ob_get_clean();
 }
